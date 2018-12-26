65047b10dbac318d695db25c3af8767115f266eb
Imam Hossain <imam2889@gmail.com>
2018-12-26 12:04:59 +0600

    tetview added

--- app/src/main/java/lab1/sdmgap/edu/imageview/MainActivity.java
+++ app/src/main/java/lab1/sdmgap/edu/imageview/MainActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button submited;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
